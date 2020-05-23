ba3759e32320d1cf8aa9ca2e1f3f75178569094f
Emeka Mbah <frankemeks77@yahoo.com>
2020-05-23 19:12:34 +0100

    ft: Install and setup Vue.js
    Install and configure Vue.js Javascript Framework
    
    Fixes: #14

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json'],
-  rootDir: '__tests__',
-  testRegex: ['.spec.js$', '.test.js$'],
-  coverageDirectory: './coverage',
-  testEnvironment: 'node',
-};
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: [
+    'js',
+    'vue',
+    'json'
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ]
+}
